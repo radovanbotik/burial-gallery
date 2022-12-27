ba0eae5c38c49af0d0f2c1833f30db92a3588b1e
radovanbotik <radovanbotik@gmail.com>
2022-12-27 19:05:08 +0100

    ''

--- src/context/SpotifyContext.jsx
+++ src/context/SpotifyContext.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useContext, useState, useEffect } from "react";
+import axios from "axios";
+
 const client_id = import.meta.env.VITE_CLIENT_ID;
 const client_secret = import.meta.env.VITE_CLIENT_SECRET;
 const dataContext = React.createContext();
@@ -9,42 +11,36 @@ const SpotifyContext = ({ children }) => {
   const [albums, setAlbums] = useState();
   useEffect(() => {
     const getToken = async () => {
-      const options = {
-        method: "POST",
+      const resp = await axios("https://accounts.spotify.com/api/token", {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
           Authorization: "Basic " + btoa(client_id + ":" + client_secret),
-          Accept: "application/json",
         },
-        // body: "grant_type=client_credentials",
-        body: "grant_type=client_credentials",
-      };
-      const resp = await fetch(
-        "https://accounts.spotify.com/api/token",
-        options
-      );
-      const data = await resp.json();
-      console.log(data);
-      setToken(data.access_token);
-      return data.access_token;
+        data: "grant_type=client_credentials",
+        method: "POST",
+      });
+
+      console.log(resp.data.access_token);
+      // setToken(data.access_token);
+      // return data.access_token;
     };
-    const getArtist = async () => {
-      const tokendata = await getToken();
-      const options = {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + tokendata,
-        },
-      };
-      console.log(options);
-      const resp = await fetch(
-        "https://api.spotify.com/v1/artists/0uCCBpmg6MrPb1KY2msceF/albums?include_groups=single&market=SK",
-        options
-      );
-      const data = await resp.json();
-      setAlbums(data);
-    };
-    getArtist();
+    getToken();
+    // const getArtist = async () => {
+    //   const tokendata = await getToken();
+    //   const options = {
+    //     method: "GET",
+    //     headers: {
+    //       Authorization: "Bearer " + tokendata,
+    //     },
+    //   };
+    //   const resp = await fetch(
+    //     "https://api.spotify.com/v1/artists/0uCCBpmg6MrPb1KY2msceF/albums?include_groups=single&market=SK",
+    //     options
+    //   );
+    //   const data = await resp.json();
+    //   setAlbums(data);
+    // };
+    // getArtist();
   }, []);
 
   // useEffect(() => {
