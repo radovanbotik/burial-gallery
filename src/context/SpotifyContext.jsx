57f679ad36563d1b51b2b85ac17addf74d52d6a7
radovanbotik <radovanbotik@gmail.com>
2022-12-27 17:36:32 +0100

    ''

--- src/context/SpotifyContext.jsx
+++ src/context/SpotifyContext.jsx
@@ -6,42 +6,27 @@ const dataContext = React.createContext();
 const SpotifyContext = ({ children }) => {
   const [token, setToken] = useState();
   const [albums, setAlbums] = useState();
-  useEffect(() => {
-    const getToken = async () => {
-      const client_id = import.meta.env.VITE_CLIENT_ID;
-      const client_secret = import.meta.env.VITE_CLIENT_SECRET;
-      const options = {
-        method: "POST",
-        headers: {
-          Authorization: "Basic " + btoa(client_id + ":" + client_secret),
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: "grant_type=client_credentials",
-      };
-      const resp = await fetch(
-        "https://accounts.spotify.com/api/token",
-        options
-      );
-      const tokendata = await resp.json();
-      setToken(tokendata.access_token);
-      const getArtist = async () => {
-        const options = {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer " + tokendata,
-          },
-        };
-        const resp = await fetch(
-          "https://api.spotify.com/v1/artists/0uCCBpmg6MrPb1KY2msceF/albums?include_groups=single&market=SK",
-          options
-        );
-        const data = await resp.json();
-        setAlbums(data);
-      };
-      getArtist();
-    };
-    getToken();
-  }, []);
+  // useEffect(() => {
+  //   const getToken = async () => {
+  //     const client_id = import.meta.env.VITE_CLIENT_ID;
+  //     const client_secret = import.meta.env.VITE_CLIENT_SECRET;
+  //     const options = {
+  //       method: "POST",
+  //       headers: {
+  //         Authorization: "Basic " + btoa(client_id + ":" + client_secret),
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       },
+  //       body: "grant_type=client_credentials",
+  //     };
+  //     const resp = await fetch(
+  //       "https://accounts.spotify.com/api/token",
+  //       options
+  //     );
+  //     const data = await resp.json();
+  //     setToken(data.access_token);
+  //   };
+  //   getToken();
+  // }, []);
 
   // useEffect(() => {
   //   const getArtist = async () => {
@@ -60,6 +45,40 @@ const SpotifyContext = ({ children }) => {
   //   };
   //   getArtist();
   // }, [token]);
+
+  useEffect(() => {
+    const client_id = import.meta.env.VITE_CLIENT_ID;
+    const client_secret = import.meta.env.VITE_CLIENT_SECRET;
+    (() => {
+      fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          Authorization: "Basic " + btoa(client_id + ":" + client_secret),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: "grant_type=client_credentials",
+      })
+        .then(resp => resp.json())
+        .then(data => {
+          setToken(data);
+          return data;
+        })
+        .then(
+          data =>
+            fetch(
+              "https://api.spotify.com/v1/artists/0uCCBpmg6MrPb1KY2msceF/albums?include_groups=single&market=SK"
+            ),
+          {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        )
+        .then(resp => resp.json())
+        .then(data => setAlbums(data));
+    })();
+  }, []);
   return (
     <dataContext.Provider value={{ albums }}>{children}</dataContext.Provider>
   );
